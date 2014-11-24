f3056c339c6a87d831353528671fc688c33d7d88
張家瑋 <pc0805a@gmail.com>
2014-11-25 04:22:33 +0800

    GPS TEST 2 (some problem)
    Can't keep running GPS Positioning in background
    (要先把定位關掉開APP在打開定位, 因為只會在onCreate做一次, 那時還沒定位完成, 所以會顯示無法定位)

--- src/com/example/weather/MainActivity.java
+++ src/com/example/weather/MainActivity.java
@@ -21,6 +21,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -34,56 +35,72 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener {
 
 	protected static final int ACTIVITY_REPORT = 1000;
+	
+	private static final int MIN_TIME_BW_UPDATES = 1000;
+	private static final int MIN_DISTANCE_CHANGE_FOR_UPDATES = 1000;
+	
+	
 	private static final String TAG = MainActivity.class.getSimpleName();
 	private boolean getService = false;
-	private LocationManager lms;
+	private LocationManager locationManager;
 	private String bestProvider = LocationManager.GPS_PROVIDER;
 
+	private boolean isNetworkEnabled;
+	private boolean isGPSEnabled;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 		LocationManager status = (LocationManager) (this
 				.getSystemService(Context.LOCATION_SERVICE));
-		if (status.isProviderEnabled(LocationManager.GPS_PROVIDER)
-				|| status.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+		isNetworkEnabled = status
+				.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+		isGPSEnabled = status.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+		if (isGPSEnabled || isGPSEnabled) {
 			// 如果GPS或網路定位開啟，呼叫locationServiceInitial()更新位置
 			locationServiceInitial();
 		} else {
 			Toast.makeText(this, "請開啟定位服務", Toast.LENGTH_LONG).show();
 			getService = true; // 確認開啟定位服務
-			startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+			startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)); // 開啟設定頁面
 		}
-		initViews();
 
+		initViews();
 		setListeners();
+		locationServiceInitial();
 
 	}
 
 	private void locationServiceInitial() {
-		lms = (LocationManager) getSystemService(LOCATION_SERVICE);	
-		Criteria criteria = new Criteria();	
-		bestProvider = lms.getBestProvider(criteria, true);	
-		Location location = lms.getLastKnownLocation(bestProvider);
-		getLocation(location);
+		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE); // 取得系統定位服務
+		Criteria criteria = new Criteria(); // 資訊提供者選取標準
+
+		bestProvider = locationManager.getBestProvider(criteria, true); // 選擇精準度最高的提供者
+		Location location = locationManager.getLastKnownLocation(bestProvider);
+		
+		
 
 		getLocation(location);
 	}
-	
-	private void getLocation(Location location) { //將定位資訊顯示在畫面中
-        if(location != null) {
-             Double longitude = location.getLongitude();   //取得經度
-             Double latitude = location.getLatitude();     //取得緯度
-             longitude_txt.setText(String.valueOf(longitude));
-             latitude_txt.setText(String.valueOf(latitude));
-        }
-        else {
-             Toast.makeText(this, "無法定位座標", Toast.LENGTH_LONG).show();
-        }
-    }
+
+	private void getLocation(Location location) { // 將定位資訊顯示在畫面中
+		
+		
+		if (location != null) {
+			Double longitude = location.getLongitude(); // 取得經度
+			Double latitude = location.getLatitude(); // 取得緯度
+			longitude_txt.setText(String.valueOf(longitude));
+			latitude_txt.setText(String.valueOf(latitude));
+		} else {
+			Toast.makeText(this, "無法定位座標", Toast.LENGTH_LONG).show();
+		}
+	}
 
 	private void getInfo(String query) {
 		String baseUrl = "https://query.yahooapis.com/v1/public/yql?q=";
@@ -221,4 +238,44 @@ public class MainActivity extends Activity {
 		dialog.show();
 	}
 
+	@Override
+	public void onLocationChanged(Location location) { // 當地點改變時
+		// TODO 自動產生的方法 Stub
+		getLocation(location);
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {// 當GPS或網路定位功能關閉時
+		// TODO 自動產生的方法 Stub
+		Toast.makeText(this, "請開啟gps或3G網路", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) { // 當GPS或網路定位功能開啟
+		// TODO 自動產生的方法 Stub
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) { // 定位狀態改變
+		// TODO 自動產生的方法 Stub
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		if (getService) {
+			locationManager.requestLocationUpdates(bestProvider, 1000, 1, this);
+			// 服務提供者、更新頻率60000毫秒=1分鐘、最短距離、地點改變時呼叫物件
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		if (getService) {
+			locationManager.removeUpdates(this); // 離開頁面時停止更新
+		}
+	}
 }
