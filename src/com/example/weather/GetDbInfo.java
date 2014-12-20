8e6f8b6f0f2aa87a3e8db86ff5a35c89a30137a9
張家瑋 <pc0805a@gmail.com>
2014-12-20 23:00:07 +0800

    get DB info success

--- src/com/example/weather/GetDbInfo.java
+++ src/com/example/weather/GetDbInfo.java
@@ -1,67 +1,95 @@
 package com.example.weather;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+
 import android.os.AsyncTask;
+import android.provider.DocumentsContract.Document;
 import android.util.Log;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
 
 public class GetDbInfo extends AsyncTask<Void, Void, String[]> {
-	
+
 	private static final String TAG = GetDbInfo.class.getSimpleName();
-	
-	private String URI = "http://pc0805a.lionfree.net/weather/getDbInfo.php";
+
+	private String baseURL = "http://pc0805a.lionfree.net/weather/getDbInfo.php?";
 	int woeid;
 	private String[] result;
 
 	GetDbInfo(String woeid) {
 		this.woeid = Integer.parseInt(woeid);
 	}
-//	http://j796160836.pixnet.net/blog/post/28994669-%5Bandroid%5D-%E4%BD%BF%E7%94%A8http%E7%9A%84post%E6%96%B9%E5%BC%8F%E5%92%8C%E7%B6%B2%E9%A0%81%E8%A1%A8%E5%96%AE%E6%BA%9D%E9%80%9A
+
 	@Override
 	protected String[] doInBackground(Void... params) {
 
+		String link = "http://pc0805a.lionfree.net/weather/getDbInfo.php?woeid="
+				+ woeid;
+
+		DefaultHttpClient httpclient = new DefaultHttpClient(
+				new BasicHttpParams());
+		HttpPost httppost = new HttpPost(link);
+
+		InputStream inputStream = null;
+
 		try {
-			URL url = new URL(URI);
-			String data = URLEncoder.encode("woeid", "UTF-8") + "="
-					+ woeid;
-			URLConnection conn = url.openConnection();
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
 
-			OutputStreamWriter wr = new OutputStreamWriter(
-					conn.getOutputStream());
+			inputStream = entity.getContent();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					inputStream, "UTF-8"), 8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
 
-//			wr.write(data);
+			if (Debug.on) {
+				Log.v(TAG, "Original Page Info: " + sb.toString());
+			}
 
-//			BufferedReader reader = new BufferedReader(new InputStreamReader(
-//					conn.getInputStream()));
-//			
-//			StringBuilder sb = new StringBuilder();
-//
-//			String line = null;
-//			while ((line = reader.readLine()) != null) {
-//				sb.append(line + "\n");
+			org.jsoup.nodes.Document doc = Jsoup.parse(sb.toString());
+
+			String plainText = doc.body().text();
+//			if (Debug.on) {
+				Log.v(TAG, "Plain Text: " + plainText);
 //			}
-//			String tempResult = sb.toString();
-//			
-//			Log.v(TAG, tempResult);
-			
 
-		} catch (MalformedURLException err) {
-			Log.e(TAG, "error: "+err.toString());
-		} catch (UnsupportedEncodingException err) {
-			Log.e(TAG, "error: "+err.toString());
-		} catch (IOException err) {
-			Log.e(TAG, "error: "+err.toString());
+		} catch (Exception err) {
+			Log.e(TAG, "error: " + err.toString());
+		} finally {
+			try {
+				if (inputStream != null)
+					inputStream.close();
+			} catch (Exception err) {
+				if (Debug.on) {
+					Log.e(TAG, "error: " + err.toString());
+				}
+			}
 		}
 
 		return null;
-	}
 
+	}
 }
